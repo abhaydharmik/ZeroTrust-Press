b062bef1486c9c315002cb23bf53f386e61926fd
unknown <dharmikabhay362@gmail.com>
2026-07-04 00:33:02 +0530

    feat: initialize React frontend with project structure and API services

---
+++ client/src/services/authService.js
@@ -0,0 +1,11 @@
+import axiosInstance from "../utils/axiosInstance";
+
+export const registerUser = (userData) =>
+  axiosInstance.post("/auth/register", userData);
+
+export const loginUser = (userData) =>
+  axiosInstance.post("/auth/login", userData);
+
+export const getProfile = () =>
+  axiosInstance.get("/auth/profile");
+
---
+++ client/src/services/blogService.js
@@ -0,0 +1,30 @@
+import axiosInstance from "../utils/axiosInstance";
+
+export const getBlogs = (params) => 
+    axiosInstance.get("/blogs", {params})
+
+export const getBlogById = (id) => 
+    axiosInstance.get(`/blogs/${id}`)
+
+export const createBlog = (formData) => 
+    axiosInstance.post("/blogs", formData)
+
+export const updateBlog = (id, formData) => 
+    axiosInstance.put(`/blogs/${id}`, formData)
+
+export const deleteBlog = (id) => 
+    axiosInstance.delete(`/blogs/${id}`)
+
+export const likeBlog = (id) => 
+    axiosInstance.put(`/blogs/${id}/like`)
+
+export const addComment = (id, text) => 
+    axiosInstance.post(`/blogs/${id}/comment`, {text})
+
+export const getMyBlogs = () => 
+    axiosInstance.get(`/blogs/my-blogs`)
+
+export const getDashboard = () => 
+    axiosInstance.get(`/blogs/dashboard`)
+
+
